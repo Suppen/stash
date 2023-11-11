c02889d9774384982ca7188af264776246e4569d
Simen de Lange <simen.de.lange@gmail.com>
2023-11-11 21:47:12 +0100

    Improved to/from sql for quantity

--- src/infrastructure/sqlite/db.rs
+++ src/infrastructure/sqlite/db.rs
@@ -71,7 +71,15 @@ impl FromSql for ProductId {
 
 impl ToSql for Quantity {
     fn to_sql(&self) -> Result<rusqlite::types::ToSqlOutput<'_>, rusqlite::Error> {
-        Ok(ToSqlOutput::from(self.value() as i64))
+        let val = self.value();
+
+        if val > i64::MAX as u64 {
+            return Err(rusqlite::Error::ToSqlConversionFailure(
+                format!("Quantity {} is too large to fit in an i64", val).into(),
+            ));
+        }
+
+        Ok(ToSqlOutput::from(val as i64))
     }
 }
 
@@ -79,9 +87,13 @@ impl FromSql for Quantity {
     fn column_result(
         value: rusqlite::types::ValueRef<'_>,
     ) -> Result<Self, rusqlite::types::FromSqlError> {
-        let int = value.as_i64()?;
+        let val = value.as_i64()?;
 
-        Quantity::new(int as u64).map_err(|_| rusqlite::types::FromSqlError::InvalidType)
+        if val < 0 {
+            return Err(rusqlite::types::FromSqlError::InvalidType);
+        }
+
+        Ok(Quantity::new(val as u64).map_err(|_| rusqlite::types::FromSqlError::InvalidType)?)
     }
 }
 
@@ -159,5 +171,42 @@ mod tests {
         let quantity_from_sql: Quantity = row.get(0).unwrap();
 
         assert_eq!(quantity, quantity_from_sql);
+    }
+
+    #[test]
+    fn test_quantity_to_sql_overflow() {
+        let quantity = Quantity::new(u64::MAX).unwrap();
+
+        let connection = rusqlite::Connection::open_in_memory().unwrap();
+        connection
+            .execute("CREATE TABLE test (quantity INTEGER NOT NULL)", params![])
+            .unwrap();
+
+        let result =
+            connection.execute("INSERT INTO test (quantity) VALUES (?)", params![quantity]);
+
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_quantity_from_sql_overflow() {
+        let connection = rusqlite::Connection::open_in_memory().unwrap();
+        connection
+            .execute("CREATE TABLE test (quantity INTEGER NOT NULL)", params![])
+            .unwrap();
+
+        connection
+            .execute("INSERT INTO test (quantity) VALUES (-1)", params![])
+            .unwrap();
+
+        let mut statement = connection
+            .prepare("SELECT quantity FROM test LIMIT 1")
+            .unwrap();
+        let mut rows = statement.query(params![]).unwrap();
+
+        let row = rows.next().unwrap().unwrap();
+        let quantity_from_sql = row.get::<_, Quantity>(0);
+
+        assert!(quantity_from_sql.is_err());
     }
 }
