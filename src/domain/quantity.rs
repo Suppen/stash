a9aa98b49c3b2a299a17a6001ded1d2bf1e6dbba
Simen de Lange <simen.de.lange@gmail.com>
2023-11-11 20:24:59 +0100

    Quantity math

--- src/domain/quantity.rs
+++ src/domain/quantity.rs
@@ -1,3 +1,5 @@
+use std::ops::{Add, Sub};
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Quantity(i64);
 
@@ -5,6 +7,8 @@ pub struct Quantity(i64);
 pub enum QuantityError {
     /// A quantity can not be zero
     Zero,
+    /// An operation caused the quantity to overflow
+    Overflow,
 }
 
 impl Quantity {
@@ -29,19 +33,85 @@ impl Quantity {
     }
 }
 
+impl Add for Quantity {
+    type Output = Result<Quantity, QuantityError>;
+
+    fn add(self, rhs: Quantity) -> Self::Output {
+        let new_value = self
+            .value()
+            .checked_add(rhs.value())
+            .ok_or(QuantityError::Overflow)?;
+
+        Quantity::new(new_value)
+    }
+}
+
+impl Sub for Quantity {
+    type Output = Result<Quantity, QuantityError>;
+
+    fn sub(self, rhs: Quantity) -> Self::Output {
+        let new_value = self
+            .value()
+            .checked_sub(rhs.value())
+            .ok_or(QuantityError::Overflow)?;
+
+        Quantity::new(new_value)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn new_quantity() {
+    fn test_new_quantity() {
         assert_eq!(Quantity::new(1), Ok(Quantity(1)));
         assert_eq!(Quantity::new(2), Ok(Quantity(2)));
         assert_eq!(Quantity::new(3), Ok(Quantity(3)));
     }
 
     #[test]
-    fn new_quantity_zero() {
+    fn test_new_quantity_zero() {
         assert_eq!(Quantity::new(0), Err(QuantityError::Zero));
+    }
+
+    #[test]
+    fn test_add() {
+        assert_eq!(
+            Quantity::new(1).unwrap() + Quantity::new(2).unwrap(),
+            Ok(Quantity::new(3).unwrap())
+        );
+    }
+
+    #[test]
+    fn test_add_overflow() {
+        assert_eq!(
+            Quantity::new(i64::MAX).unwrap() + Quantity::new(1).unwrap(),
+            Err(QuantityError::Overflow)
+        );
+    }
+
+    #[test]
+    fn test_sub() {
+        assert_eq!(
+            Quantity::new(3).unwrap() - Quantity::new(2).unwrap(),
+            Ok(Quantity::new(1).unwrap())
+        );
+    }
+
+    #[test]
+    fn test_sub_overflow() {
+        assert_eq!(
+            Quantity::new(i64::MIN).unwrap() - Quantity::new(1).unwrap(),
+            Err(QuantityError::Overflow)
+        );
+    }
+
+    #[test]
+    fn test_sub_zero() {
+        assert_eq!(
+            Quantity::new(1).unwrap() - Quantity::new(1).unwrap(),
+            Err(QuantityError::Zero)
+        );
     }
 }
