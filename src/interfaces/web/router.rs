aea0e709f67a556a50007d0bb670496ecc98bcf0
Simen de Lange <simen.de.lange@gmail.com>
2023-11-19 12:59:28 +0100

    GetStashItemByProductIdAndExpiryDate handler

--- src/interfaces/web/router.rs
+++ src/interfaces/web/router.rs
@@ -2,7 +2,7 @@ use actix_web::web;
 
 use super::handlers::{
     delete_product_by_id, delete_stash_item_by_id, get_product_by_id, get_stash_item_by_id,
-    save_product, save_stash_item,
+    get_stash_item_by_product_id_and_expiry_date, save_product, save_stash_item,
 };
 
 pub fn configure_routes(cfg: &mut web::ServiceConfig) {
@@ -17,5 +17,9 @@ pub fn configure_routes(cfg: &mut web::ServiceConfig) {
             web::resource("/stash_item/{stash_item_id}")
                 .route(web::get().to(get_stash_item_by_id))
                 .route(web::delete().to(delete_stash_item_by_id)),
+        )
+        .service(
+            web::resource("/stash_item/product/{product_id}/expiry_date/{expiry_date}")
+                .route(web::get().to(get_stash_item_by_product_id_and_expiry_date)),
         );
 }
