e7917391acd41e9e91120282d27ceaf62da36298
Simen de Lange <simen.de.lange@gmail.com>
2023-11-27 20:57:01 +0100

    Full testing of json fetcher

--- frontend/src/utils/createJSONFetcher.test.ts
+++ frontend/src/utils/createJSONFetcher.test.ts
@@ -3,7 +3,7 @@ import { createJSONFetcher } from "./createJSONFetcher";
 
 describe("the returned function", () => {
     it("should call the fetcher with the arguments the function is called with", async () => {
-        const fetcher = vi.fn().mockResolvedValue({ json: () => Promise.resolve({}) });
+        const fetcher = vi.fn().mockResolvedValue(Response.json({}));
         const url = "url";
         const options = { method: "POST" };
 
@@ -16,22 +16,61 @@ describe("the returned function", () => {
     it("should call the parser with the parsed JSON data", async () => {
         const parser = vi.fn().mockReturnValue({});
         const data = { cake: "Cheese" };
-        const fetcher = vi.fn().mockResolvedValue({ json: () => Promise.resolve(JSON.stringify(data)) });
+        const fetcher = vi.fn().mockResolvedValue(Response.json(data));
 
         const jsonFetcher = createJSONFetcher(parser, fetcher);
 
         await jsonFetcher();
-        expect(parser).toHaveBeenCalledWith(JSON.stringify(data));
+        expect(parser).toHaveBeenCalledWith(data);
     });
 
     it("should return what the parser returns", async () => {
         const expected = {};
         const parser = vi.fn<[], Record<string, unknown>>().mockReturnValue(expected);
-        const fetcher = vi.fn().mockResolvedValue({ json: () => Promise.resolve({}) });
+        const fetcher = vi.fn().mockResolvedValue(Response.json({}));
 
         const jsonFetcher = createJSONFetcher(parser, fetcher);
 
         const actual = await jsonFetcher();
         expect(actual).toBe(expected);
+    });
+
+    it("should throw a network error if the request fails", async () => {
+        const fetcher = vi.fn().mockRejectedValue(new Error("Network error"));
+
+        const jsonFetcher = createJSONFetcher(vi.fn(), fetcher);
+
+        await expect(jsonFetcher()).rejects.toThrow("Network error");
+    });
+
+    it("should throw the response if the response is not ok", async () => {
+        const response = Response.error();
+        const fetcher = vi.fn().mockResolvedValue(response);
+
+        const jsonFetcher = createJSONFetcher(vi.fn(), fetcher);
+
+        await expect(jsonFetcher()).rejects.toBe(response);
+    });
+
+    it("should throw a JSON parse error if the response is not valid JSON", async () => {
+        const fetcher = vi.fn().mockResolvedValue({
+            ok: true,
+            json: () => Promise.resolve().then(() => JSON.parse("Not JSON") as unknown)
+        });
+
+        const jsonFetcher = createJSONFetcher(vi.fn(), fetcher);
+
+        await expect(jsonFetcher()).rejects.toThrow();
+    });
+
+    it("should throw whatever error the parser throws", async () => {
+        const parser = vi.fn().mockImplementation(() => {
+            throw new Error("Parser error");
+        });
+        const fetcher = vi.fn().mockResolvedValue(Response.json({}));
+
+        const jsonFetcher = createJSONFetcher(parser, fetcher);
+
+        await expect(jsonFetcher()).rejects.toThrow("Parser error");
     });
 });
--- frontend/src/utils/createJSONFetcher.ts
+++ frontend/src/utils/createJSONFetcher.ts
@@ -6,6 +6,11 @@
  *
  * @returns Function that takes the same arguments as `fetch`, and returns a promise with JSON data parsed to the
  * wanted type
+ *
+ * @throws Network error, if the request fails
+ * @throws The response, if the response is not ok
+ * @throws JSON parse error, if the response is not valid JSON
+ * @throws Whatever error the parser throws
  */
 export function createJSONFetcher<T, FP extends Parameters<typeof fetch>>(
     parser: (data: unknown) => T,
@@ -13,6 +18,11 @@ export function createJSONFetcher<T, FP extends Parameters<typeof fetch>>(
 ) {
     return async (...args: FP): Promise<T> => {
         const response = await fetcher(...args);
+
+        if (!response.ok) {
+            throw response;
+        }
+
         const data = (await response.json()) as unknown;
         return parser(data);
     };
